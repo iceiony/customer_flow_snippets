cee833804c7c04f8585452e8297ea8f0e1925611
iceiony <iceiony@yahoo.com>
2017-02-01 17:52:57 +0000

    read data from payment file and calculate number of days

--- init.R
+++ init.R
@@ -3,6 +3,13 @@ library('plyr')
 library('dplyr')
 library('reshape2')
 library('ggplot2')
+library('parallel')
+library('tictoc')
 
 # source all functions from mlp_functions folder
 sources <- lapply(list.files('functions', '[.]R$', full.names = T), source)
+
+elapsed <- function(){
+    toc()
+    tic()
+}
--- show_data.R
+++ show_data.R
@@ -1,8 +1,26 @@
 source('init.R')
 
-shop_sales <- read.csv('./data/prediction_example.csv', sep = ',', header = FALSE) 
-colnames(shop_sales) <- c('shop_id', 1:14)
+#shop_sales <- read.table('./data/prediction_example.csv', sep = ',',
+#                         col.names = c('shop_id', 1:14)) 
+#
+#plot_sales(shop_sales, c(1,3))
+tic();
+all_sales <- read.table('./data/sample_pay.txt', sep = ',', 
+                        col.names  = c('user_id', 'shop_id', 'time_stamp'),
+                        colClasses = c('integer', 'integer', 'Date'))
+message('Data Read')
+elapsed()
 
-plot_sales(shop_sales, c(1,3))
+batches  <- 1:nrow(all_sales) %/% 10000
+start_date <- as.Date('2015-07-01')
+all_sales$nr_days <- unlist(
+                        mclapply(split(all_sales$time_stamp, batches), 
+                                 function(x) as.numeric(x - start_date)))
+message('Days counted')
+elapsed()
+
+all_sales <- all_sales[order(all_sales$nr_days), ]
+message('Records sorted')
+elapsed()
 
 
