719ed760985d5aa33fd3d2a377a20ee27e7bf889
iceiony <iceiony@yahoo.com>
2017-02-09 17:38:41 +0000

    plot fourier domain for single shop

---
+++ show_furier.R
@@ -0,0 +1,20 @@
+source('./init.R')
+
+shop_sales  <- read.table('./data/daily_shop_sales.csv', sep = ',', header = T)
+shop_groups <- read.table('./data/shop_clusters.csv', sep = ',', header = T)
+
+sales <- as.matrix2(shop_sales)
+sales[is.na(sales)] <- 0
+
+
+faze <- 365
+L  <- length(sales[1,-1]) 
+hz <- c(0 , seq(L/2))
+hz <- hz * faze / L
+shop1 <- abs(fft(sales[1,-1])) / L
+plot(hz, shop1[seq_along(hz)], 'l')
+#clean <- fft(shop1, inverse = T)
+plot_series(sales, 1)
+
+
+
