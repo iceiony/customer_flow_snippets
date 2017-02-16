bf7f9b84706f97597a67ae27ca7c95be80f0b0c5
iceiony <iceiony@yahoo.com>
2017-02-16 13:52:51 +0000

    predict from fourier with trend

--- fourier_predict.R
+++ fourier_predict.R
@@ -2,16 +2,11 @@ library('purrr')
 
 period <- 2 * pi
 f0 <- pi / 100
-x <- seq(0, period, pi/100)
+x <- seq(0, 3*period, pi/100)
 x <- x[-1]
 y <- 1.2 * sin(3 * x + pi/4) + 0.55 * sin(10 * x) + 2 
 
 trend <- lm(y~x)
-plot(x,y,'l')
-abline(trend,col='red')
-dev.new()
-plot(trend$residuals,,'l')
-
 fourier <- fft(trend$residuals)
 duration <- 3
 sample_freq <- y
@@ -30,12 +25,18 @@ ampli <- ampli[imp]
 phase <- phase[imp]
 freq <- freq[imp]
 
-#freq  <- (seq_along(fourier)) * base_freq
-
-#moments <- c(seq(L)-1,seq(L,L*2))
 
 res <- sapply(seq(L*2) - 1, function(t){
    a0 + sum(ampli * sin(freq * f0 * t + phase))
 })
 plot(res,,'l',col='red',lwd=3)
 lines(trend$residuals,,'l')
+
+#try predicting the future
+x <- seq(0, 6*period, pi/100)
+x <- x[-1]
+y <- 1.2 * sin(3 * x + pi/4) + 0.55 * sin(10 * x) + 2 
+res <- res + predict(trend, newdata = list(x))
+plot(x,res,'l',col='red',lwd=3)
+lines(x,y)
+abline(trend,,col='blue')
