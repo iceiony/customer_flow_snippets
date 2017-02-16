d66bff2ee0dc471d73f84bdbf6d24abb8c425735
iceiony <iceiony@yahoo.com>
2017-02-16 19:19:02 +0000

    demonstrate fourier prediction defects for missing data

--- fourier_predict.R
+++ fourier_predict.R
@@ -1,18 +1,23 @@
+source('./init.R')
 library('purrr')
 
 period <- 2 * pi
 f0 <- pi / 100
-x <- seq(0, 3*period, pi/100)
+x <- seq(0, 1.5*period, pi/100)
 x <- x[-1]
 y <- 1.2 * sin(3 * x + pi/4) + 0.55 * sin(10 * x) + 2 
 
 trend <- lm(y~x)
+pad_size <- length(x) %% (period/f0)
+y <- c(y, rep(mean(y),pad_size))
 fourier <- fft(y)
 
 L <- length(fourier)
 
 a0 <- Mod(fourier[1]) / L
+a0 <- 2
 fourier <- fourier[seq(2, L/2)] * 2 / L
+#fourier <- fourier[seq(2, period/f0)] * 2 / L
 ampli <- unname(Mod(fourier))
 phase <- unname(Arg(fourier) + pi/2)
 freq  <- seq_along(fourier) * (period/f0) / L
@@ -27,14 +32,13 @@ freq <- freq[imp]
 res <- sapply(seq(L*2) - 1, function(t){
    a0 + sum(ampli * sin(freq * f0 * t + phase))
 })
-plot(res,,'l',col='red',lwd=3)
-lines(trend$residuals,,'l')
+#plot(res,,'l',col='red',lwd=3)
+#lines(trend$residuals + predict(trend),,'l')
 
 #try predicting the future
-x <- seq(0, 6*period, pi/100)
+x <- seq(0, length(res)*pi/100, pi/100)
 x <- x[-1]
 y <- 1.2 * sin(3 * x + pi/4) + 0.55 * sin(10 * x) + 2 
-res <- res + predict(trend, newdata = list(x))
+res <- res
 plot(x,res,'l',col='red',lwd=3)
 lines(x,y)
-abline(trend,,col='blue')
