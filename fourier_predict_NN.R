5e75a72423ba2d6d46e17ff34c69093eb2c14477
iceiony <iceiony@yahoo.com>
2017-02-17 21:50:26 +0000

    do network training in one step

--- fourier_predict_NN.R
+++ fourier_predict_NN.R
@@ -10,6 +10,7 @@ y <- 1.2 * sin(3 * x + pi/4) + 0.55 * sin(10 * x) + 2
 TARG <- t(t(y))
 #add noise to the sequence
 noise_idx <- sample(seq_along(y), length(y) * 0.3)
+#noise_idx <- round(seq(3, length(y), 2.2))
 y[noise_idx] <- 0
 y <- c(y, rep(0,100))
 #y[200:300]  <- 0
@@ -26,24 +27,28 @@ ampli <- unname(Mod(fourier))
 phase <- unname(Arg(fourier) + pi/2)
 freq  <- seq_along(fourier) * (period/f0) / L
 
-IN <- sapply(seq(length(x))-1, 
+generate_in <- function(size){
+    sapply(seq(size) - 1, 
          function(t){
-             sin(freq * f0 * t + phase)
-      }) %>% t()
-IN <- cbind(IN,a0)
+             x <- freq * f0 * t
+             y <- phase
+             # sin(x+y) = sin(x)cos(y) + cos(x)sin(y)
+             # sin(freq * f0 * t + phase)
+             c(sin(x+y))
+      }) %>% t() %>% cbind(a0)
+}
 
-hidden <- c()
-net <- train_network(IN, TARG, hidden, c(.0012), 500)
+IN <- generate_in(length(x))
 
-plot(net$errors, type = 'l', ylim = c(0, max(net$errors)), xlim=c(0,500))
-message('Training error : ' , last(net$errors))
+IN <- cbind(1, IN)
+net <- list(w = list(pseudoinverse(IN) %*% TARG))
+#hidden <- c(100)
+#net <- train_network(IN, TARG, hidden, c(.00019,.00014), 500)
+#plot(net$errors, type = 'l', ylim = c(0, max(net$errors)), xlim=c(0,500))
+#message('Training error : ' , last(net$errors))
 
 #try predicting the future
-IN <- sapply(seq(length(x) * 2) - 1, 
-         function(t){
-             sin(freq * f0 * t + phase)
-      }) %>% t()
-IN <- cbind(IN,a0)
+IN <- generate_in(length(x) * 2)
 
 res <- last(run_network(IN,net$w))
 x <- seq(0, length(res) * pi/100, pi/100)
