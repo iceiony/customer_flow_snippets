9b681a73e1a066541f8a1d80400afd307ad1cdfa
iceiony <iceiony@yahoo.com>
2017-03-10 17:10:39 +0000

    update pad for testing refactored code

--- pad.R
+++ pad.R
@@ -11,7 +11,7 @@ nets <- mclapply(seq(24), mc.cores = 8,
              function(idx){
                 shop_id <- 3
                 
-                views <- unlist(shop_views[shop_id, -1])
+                views <- unlist(shop_views[shop_id, -1]) 
                 sales <- unlist(shop_sales[shop_id, -1])
 
                 chop <- sample(21, 1) - 1
@@ -20,39 +20,45 @@ nets <- mclapply(seq(24), mc.cores = 8,
                     sales <- head(sales, -chop)
                 }
 
-                net <- mlp_train(views, sales, pre_views, pre_sales)
-                net$shop_id <- shop_id
-
-                plot(net$errors, type = 'l', ylim = c(0, 100) , xlim=c(0, 1000))
-                net$train_error
-
                 valid <- tail(sales, 14) 
                 views <- head(views, -14)
                 sales <- head(sales, -14)
 
-                pred  <- mlp_predict(views, sales, pre_views, pre_sales, net, 14)
+                net <- mlp_train(views, sales, pre_views, pre_sales)
+                net$shop_id <- shop_id
+                #plot(net$errors, type = 'l', ylim = c(0, 100) , xlim=c(0, 1000))
+                #net$train_error
 
-                plot_series(rbind(pred, valid))
-                report_error(pred,valid)
+                pred  <- mlp_predict(views, sales, pre_views, pre_sales, net, 14)
+                net$score <- stats_err(pred, valid)
+                #plot_series(rbind(pred, valid))
+                #report_error(pred,valid)
 
                 return(net)
              }) 
 
-scores <- ldply(nets, function(net){
-                views <- shop_views[net$shop_id, -1]
-                sales <- shop_sales[net$shop_id, -1]
-                pred  <- mlp_predict(views, sales, pre_views, pre_sales, net)
 
-                pred  <- tail(pred, 14)
-                valid <- tail(views, 14)
+predictions <- ldply(nets, function(net){
+                    views <- unlist(shop_views[net$shop_id, -1])
+                    sales <- unlist(shop_sales[net$shop_id, -1])
+                    pred  <- mlp_predict(views, sales, pre_views, pre_sales, net, 14)
+                    valid <- tail(sales, 14)
+                    error <- stats_err(pred, valid)$err
 
-                stats_err(unlist(pred), valid)$err
-          })
+                    #dev.hold()
+                    #plot_series(rbind(pred, valid))
+                    #dev.flush()
+
+                    data.frame(error, data = I(list(pred)))
+              })
+
+scores <- predictions$error
 dim(scores) <- c(length(scores), 1)
 
-avg_pred <- apply(preds, 2, function(pred){
+avg_pred <- laply(predictions$data, identity) %>%
+            apply(2, function(pred){
                 mean(pred)
-          })
+            })
 
 print('Avg error')
 report_error(avg_pred, valid)
